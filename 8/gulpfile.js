1f1b9b0b7e6462bca4cb329f180605c33cf03c01
Keks <keks@htmlacademy.ru>
2023-02-19 08:25:57 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/gulpfile.js
@@ -0,0 +1,69 @@
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import csso from "postcss-csso";
+import rename from "gulp-rename";
+import { stacksvg } from "gulp-stacksvg";
+import svgo from "gulp-svgmin";
+import { htmlValidator } from "gulp-w3c-html-validator";
+import browser from "browser-sync";
+
+const server = browser.create();
+const { src, dest, watch, series, parallel } = gulp;
+
+const Path = {
+	STYLES: ["./styles/**/*.css", "!./styles/**/*.min.css"],
+	ICONS: ["./icons/**/*.svg", "!./icons/stack.svg"]
+}
+
+export function processStyles() {
+	return src(Path.STYLES)
+		.pipe(plumber())
+		.pipe(postcss([autoprefixer(), csso()]))
+		.pipe(
+			rename({
+				extname: ".min.css",
+			})
+		)
+		.pipe(dest("./styles/"))
+		.pipe(server.stream());
+}
+
+export function createStack() {
+	return src(Path.ICONS)
+		.pipe(svgo())
+		.pipe(stacksvg())
+		.pipe(dest("./icons"));
+}
+
+export function validateMarkup() {
+	return src("./*.html")
+		.pipe(htmlValidator.analyzer())
+		.pipe(htmlValidator.reporter({ throwErrors: true }));
+}
+
+export function startServer(done) {
+	server.init({
+		server: {
+			baseDir: "./",
+		},
+		cors: true,
+		notify: false,
+		ui: false,
+	});
+	done();
+}
+
+function reloadServer() {
+	return server.reload();
+}
+
+function watchFiles() {
+	watch(Path.STYLES, processStyles);
+	watch(Path.ICONS, series(createStack, reloadServer));
+	watch("./**/*.{html,js,jpg,png,svg,ico,webmanifest}", reloadServer);
+}
+
+export const compileProject = parallel(processStyles, createStack);
+export const runDev = series(compileProject, startServer, watchFiles);
